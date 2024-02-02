1b8659a1ab12d7df8e4d3e989f64738b4c373546
Skye Sprung <skye.sprung@gmail.com>
2024-02-02 10:58:12 +0100

    more command line functions (show-all, remove)

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,7 @@ import  qualified Data.UnixTime (UnixTime(..), getUnixTime)
 import qualified Tasks
 import qualified Config
 import Foreign.C (CTime(..))
+import Text.Read (readMaybe)
 
 -- from https://stackoverflow.com/questions/16191824/index-of-element-in-list-in-haskell
 mapInd :: (a -> Int -> b) -> [a] -> [b]
@@ -19,7 +20,7 @@ printTaskList tasks = do
     (Data.UnixTime.UnixTime (CTime currentUnixSeconds) _) <- Data.UnixTime.getUnixTime
     case tasks of
         Just t -> do
-            let 
+            let
                 indexed = indexize t
                 events = filter (\(task, _) -> case task of
                         Tasks.Event _ (Tasks.UnixTimeStamp endSecs) _ -> endSecs > fromIntegral currentUnixSeconds
@@ -47,20 +48,21 @@ timeStampOrZero str = case Tasks.getTimeStamp str of
 printHelp :: IO ()
 printHelp = do
     putStrLn "Usage:"
-    putStrLn "Lamtask -> print stored tasks"
+    putStrLn "Lamtask -> print current tasks"
     putStrLn "Lamtask add-todo description -> create a simple todo with description"
     putStrLn "Lamtask add-timed hh:mm/[d]d.[m]m.[yyyy] description -> create a timed todo with due date and description"
     putStrLn "Lamtask add-event hh:mm/[d]d.[m]m.[yyyy] hh:mm/[d]d.[m]m.[yyyy] description-> create a calendar-style event from the first time to the second time with description"
+    putStrLn "Lamtask show-all -> print all tasks, current or not"
+    putStrLn "Lamtask remove n -> remove task number n"
+    putStrLn "Lamtask remove all -> purge every task"
 
 main :: IO ()
 main = do
     args <- getArgs
     storedTasks <- Config.getStoredTasks "./tasks.txt"
     let
-        saveEvent event =
+        switcharoo =
             do
-                putStrLn ("Adding " <> Tasks.prettyPrint (event, 0))
-                Config.saveTasks "./tasks.txt.new" (event : fromMaybe [] storedTasks)
                 oldFileExists <- doesFileExist "./tasks.txt.old"
                 if oldFileExists then
                     do
@@ -76,6 +78,11 @@ main = do
                         do
                             putStrLn "Starting new tasks.txt..."
                 renameFile "./tasks.txt.new" "./tasks.txt"
+        saveEvent event =
+            do
+                putStrLn ("Adding " <> Tasks.prettyPrint (event, 0))
+                Config.saveTasks "./tasks.txt.new" (event : fromMaybe [] storedTasks)
+                switcharoo
     case args of
             [] ->
                 printTaskList storedTasks
@@ -90,4 +97,28 @@ main = do
                     saveEvent event
             ["add-timed", due, desc] -> do
                 saveEvent (Tasks.TimedTodo (timeStampOrZero due) desc)
+            ["show-all"] ->
+                case storedTasks of
+                    Just t -> do
+                        putStrLn $ mconcat $ map Tasks.prettyPrint $ indexize t
+                    Nothing ->
+                        putStrLn "No stored tasks."
+            ["remove", n] ->
+                let
+                    num = readMaybe n :: Maybe Int
+                in
+                    case num of
+                        Just i ->
+                                let
+                                    indexed = indexize $ fromMaybe [] storedTasks
+                                in do
+                                    putStrLn ("Removing entry " <> show i <> "...")
+                                    Config.saveTasks "./tasks.txt.new" $ map fst $ filter (\(_, idx) -> idx /= i) indexed
+                                    switcharoo
+                        Nothing ->
+                            if n == "all" then do
+                                putStrLn "Removing task list entirely."
+                                removeFile "./tasks.txt"
+                            else
+                                putStrLn "Could not parse the number you entered, make sure it is a valid index with \"Lamtask show-all\""
             _ -> printHelp
